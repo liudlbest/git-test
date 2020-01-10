208924ba5ee5f50ea4f1bcd0499d20aa72b330ae
liudlbest <liudlbest@126.com>
2020-01-10 17:53:53 +0800

    add from master

--- a2.js
+++ a2.js
@@ -4,6 +4,7 @@ a2.js
 insert a line in branch issue0531
 fix issue0531
 a2 changed
+------add from master 
 add a new line from merge-test branch, test merge
 add a line at hotfix branch, test merge method.
 add a new line, test merge method.
